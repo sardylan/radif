3db741daf26e18a69127e0e8d11a6879a7e8462e
Luca Cireddu <sardylan@gmail.com>
2025-08-20 00:40:01 +0200

    chore(license): adds license header in every source file

--- radif/src/fields/sponsored_award.rs
+++ radif/src/fields/sponsored_award.rs
@@ -1,3 +1,20 @@
+/*
+ * radif
+ * Copyright (C) 2025 - Luca Cireddu (IS0GVH) <sardylan@gmail.com>
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 use crate::data::AdifData;
 use crate::error::AdifError;
 use crate::result;
